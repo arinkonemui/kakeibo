e635a283ac67c93c817e9ae0743c56df10483187
arinko <59813518+arinkonemui@users.noreply.github.com>
2026-04-03 16:34:39 +0900

    公開用にサブディレクトリ構成を追加

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/apps/kakeibo/",
   plugins: [react()],
   server: {
     proxy: {
